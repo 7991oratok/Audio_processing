086234ba4f29e7ccd948438160baffd360c4566a
Mark Xjr <7991oratok@gmail.com>
2019-11-14 00:03:00 +0900

    リファクタリング

--- record_tmp.py
+++ record_tmp.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-#マイク0番からの入力を受ける。録音し、保存する。
-
 import pyaudio
 import sys
 import time
@@ -13,25 +11,20 @@ from datetime import datetime
 import subprocess
 
 def recognize_rec():
+
     chunk = 1024
+    threshold = 0.002
+
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
-
-    CARD = 1 #OUTPUTの指定
-    DEVICE = 0 #OUTPUTの指定
-    threshold = 0.002 #しきい値
-    #サンプリングレート、マイク性能に依存
     RATE = 16000
-    STOP_SECONDS = 1  # [sec]
-    B = int(RATE / chunk * STOP_SECONDS)
-    #録音時間の上限
-    RECORD_SECONDS = 10
+    STOP_SECONDS = 1  # レコード後の無音区間の継続時間
+    RECORD_SECONDS = 10 # レコード時間の最大値
 
-    #pyaudio
+    A = int(RATE) / int(chunk) * int(RECORD_SECONDS)
+    B = int(RATE / chunk * STOP_SECONDS)
     p = pyaudio.PyAudio()
-    #マイク0番を設定
-    input_device_index = 0
-    #マイクからデータ取得
+
     stream = p.open(format = FORMAT,
                     channels = CHANNELS,
                     rate = RATE,
@@ -39,10 +32,6 @@ def recognize_rec():
                     frames_per_buffer = chunk)
 
     
-    
-
-    A = int(RATE) / int(chunk) * int(RECORD_SECONDS)
-
     print("RATE = " + str(RATE))
     print("chunk = " + str(chunk))
     print("RECORD_SECONDS = " + str(RECORD_SECONDS))
@@ -68,11 +57,10 @@ def recognize_rec():
                 all.append(data)
                 x = np.frombuffer(data, dtype="int16") / 32768.0
                 xmax = x.max()
+
                 if xmax <= threshold:
-                    # 閾値を下回っていたらincrementする
                     n += 1
                 else:
-                    # 閾値を上回ったらリセットする
                     n = 0
                 # 中断判定
                 if n == B:
@@ -88,10 +76,7 @@ def recognize_rec():
             cnt += 1
 
             
-        if cnt > 5 : break
-
-
-        #time.sleep(0.5)         
+        if cnt > 5 : break      
 
     stream.close() 
     p.terminate()
@@ -100,4 +85,4 @@ def recognize_rec():
 if __name__ == '__main__':
     print(recognize_rec())
     print("Recording finished.")
-    subprocess.run('HCopy')
+    # subprocess.run('HCopy')
